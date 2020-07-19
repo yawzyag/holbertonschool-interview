f82c8a9481a5962872cd04aeb0a934a26925834a
yawzyag <yesid.dev93@gmail.com>
2020-07-19 21:49:51 +0000

    [Fix] negative keys

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,17 +7,13 @@ def canUnlockAll(boxes):
     if (len(boxes[0]) == 0):
         return False
     keys = set()
-    keys.update(boxes[0])
-    alreadySee = set()
+    keys.add(0)
 
     i = 0
     while i < len(boxes):
         for num in boxes[i]:
-            if i in keys and i not in alreadySee:
+            if i in keys and num < len(boxes) and num > 0:
                 keys.add(num)
-                alreadySee.add(i)
-                i = 0
-                break
         i += 1
 
     if (len(keys) == len(boxes) or len(keys) == (len(boxes) - 1)):
