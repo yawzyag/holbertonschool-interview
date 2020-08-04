6902f7b65bf99e74c8d9b9542ce81d0dae9bc46f
yawzyag <yesid.dev93@gmail.com>
2020-08-04 21:22:29 +0000

    [Feature] insert in binary tree

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -22,5 +24,8 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+int h_size(binary_tree_t *root);
+heap_t *insertion_heap(binary_tree_t *root, int value, int i, int size);
 
 #endif /* BINARY_TREES_H */
