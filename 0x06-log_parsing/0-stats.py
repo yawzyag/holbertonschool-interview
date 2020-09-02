dd02524d65fa68bd4c7faaae1ae0e9c17bdbc0f6
yawzyag <yesid.dev93@gmail.com>
2020-09-02 01:50:00 +0000

    [Feature] parsing log

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,23 +2,25 @@
 """
 Min operations
 """
-
 import sys
 
-try:
-    count = 1
-    fileSize = 0
-    arrCodes = []
-    for line in sys.stdin:
-        actualLine = line.split()
-        fileSize += int(actualLine[-1])
-        arrCodes.append(int(actualLine[-2]))
-        if (count == 10):
-            print("File size: {}".format(fileSize))
-            for item in sorted(arrCodes):
-                print("{}: {}".format(item, arrCodes.count(item)))
-            arrCodes = []
-            count = 1
-        count += 1
-except (BrokenPipeError, IOError):
-    pass
+if __name__ == "__main__":
+    try:
+        count = 1
+        fileSize = 0
+        arrCodes = []
+        for line in sys.stdin:
+            actualLine = line.split()
+            fileSize += int(actualLine[-1])
+            arrCodes.append(int(actualLine[-2]))
+            if (count == 10):
+                print("File size: {}".format(fileSize))
+                finallCodes = sorted(arrCodes)
+                for item in finallCodes:
+                    print("{}: {}".format(item, arrCodes.count(item)))
+                    finallCodes.remove(item)
+                arrCodes = []
+                count = 1
+            count += 1
+    except (BrokenPipeError, IOError):
+        pass
