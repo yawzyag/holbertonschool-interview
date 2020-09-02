508d3fa7ee6f84a62d72844cc2d66daa61efb46a
yawzyag <yesid.dev93@gmail.com>
2020-09-02 02:29:40 +0000

    [Feature] parsing log

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,7 +9,7 @@ arrCodes = []
 status = ["200", "301", "400", "401", "403", "404", "405", "500"]
 try:
     for line in sys.stdin:
-        actualLine = line.split()
+        actualLine = line.split(" ")
         if(len(actualLine) > 2):
             actualLine = line.split()
             fileSize += int(actualLine[-1])
@@ -17,19 +17,15 @@ try:
             count += 1
         if (count == 10):
             print("File size: {:d}".format(fileSize))
-            finallCodes = sorted(arrCodes)
-            for item in finallCodes:
-                if (status.count(item) != 0):
+            for item in status:
+                if (status.count(item) != 0 and arrCodes.count(item) > 0):
                     print("{:}: {:d}".format(item, arrCodes.count(item)))
-                    finallCodes.remove(item)
-            arrCodes = []
             count = 0
 except Exception:
     pass
 finally:
     print("File size: {:d}".format(fileSize))
-    finallCodes = sorted(arrCodes)
-    for item in finallCodes:
-        if (status.count(item) != 0):
+    for item in status:
+        if (status.count(item) != 0 and arrCodes.count(item) > 0):
             print("{:}: {:d}".format(item, arrCodes.count(item)))
             finallCodes.remove(item)
