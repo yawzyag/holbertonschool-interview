d8869e4e43af6217fea483efdfbc08a360d27111
yawzyag <yesid.dev93@gmail.com>
2020-09-02 02:42:31 +0000

    [Feature] parsing log

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,27 +5,32 @@ import sys
 
 count = 0
 fileSize = 0
-arrCodes = []
-status = ["200", "301", "400", "401", "403", "404", "405", "500"]
+arrCodes = [0, 0, 0, 0, 0, 0, 0, 0]
+s = ["200", "301", "400", "401", "403", "404", "405", "500"]
 try:
     for line in sys.stdin:
         actualLine = line.split(" ")
         if(len(actualLine) > 2):
-            actualLine = line.split()
+            c = actualLine[-2]
+            if c in s:
+                i = s.index(c)
+                arrCodes[i] += 1
             fileSize += int(actualLine[-1])
-            arrCodes.append(actualLine[-2])
             count += 1
         if (count == 10):
             print("File size: {:d}".format(fileSize))
-            for item in status:
-                if (status.count(item) != 0 and arrCodes.count(item) > 0):
-                    print("{:}: {:d}".format(item, arrCodes.count(item)))
+            i = 0
+            while i < len(s):
+                if (arrCodes[i] != 0):
+                    print("{:}: {:d}".format(s[i], arrCodes[i]))
+                i += 1
             count = 0
 except Exception:
     pass
 finally:
     print("File size: {:d}".format(fileSize))
-    for item in status:
-        if (status.count(item) != 0 and arrCodes.count(item) > 0):
-            print("{:}: {:d}".format(item, arrCodes.count(item)))
-            finallCodes.remove(item)
+    i = 0
+    while i < len(s):
+        if (arrCodes[i] != 0):
+            print("{:}: {:d}".format(s[i], arrCodes[i]))
+        i += 1
