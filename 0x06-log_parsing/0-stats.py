ef28b9501c71034462f698a1168542115887d8f9
yawzyag <yesid.dev93@gmail.com>
2020-09-02 02:21:01 +0000

    [Feature] parsing log

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,33 +3,33 @@
 import sys
 
 
-count = 1
+count = 0
 fileSize = 0
 arrCodes = []
-status = [200, 301, 400, 401, 403, 404, 405, 500]
+status = ["200", "301", "400", "401", "403", "404", "405", "500"]
 try:
     for line in sys.stdin:
         actualLine = line.split()
         if(len(actualLine) > 2):
             actualLine = line.split()
             fileSize += int(actualLine[-1])
-            arrCodes.append(int(actualLine[-2]))
-            if (count == 10):
-                print("File size: {}".format(fileSize))
-                finallCodes = sorted(arrCodes)
-                for item in finallCodes:
-                    if (status.count(item) != 0):
-                        print("{}: {}".format(item, arrCodes.count(item)))
-                        finallCodes.remove(item)
-                arrCodes = []
-                count = 1
+            arrCodes.append(actualLine[-2])
             count += 1
+        if (count == 10):
+            print("File size: {:d}".format(fileSize))
+            finallCodes = sorted(arrCodes)
+            for item in finallCodes:
+                if (status.count(item) != 0):
+                    print("{:}: {:d}".format(item, arrCodes.count(item)))
+                    finallCodes.remove(item)
+            arrCodes = []
+            count = 0
 except Exception:
     pass
 finally:
-    print("File size: {}".format(fileSize))
+    print("File size: {:d}".format(fileSize))
     finallCodes = sorted(arrCodes)
     for item in finallCodes:
         if (status.count(item) != 0):
-            print("{}: {}".format(item, arrCodes.count(item)))
+            print("{:}: {:d}".format(item, arrCodes.count(item)))
             finallCodes.remove(item)
