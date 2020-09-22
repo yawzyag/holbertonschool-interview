a92d62e0b204319a8cbd02e9cff4d5a4ad0f6fa2
yawzyag <yesid.dev93@gmail.com>
2020-09-22 01:56:09 +0000

    [Feature] utf-8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,14 @@ Validate utf8
 """
 
 
-def count_leading_ones(byte):
-    for i in range(8):
-        if byte >> (7 - i) == 0b11111111 >> (7 - i) & ~1:
-            return i
-    return 8
-
-
 def validUTF8(data):
     successive_10 = 0
     for b in data:
-        b = bin(b).replace('0b','').rjust(8, '0')
+        b = bin(b).replace('0b', '').rjust(8, '0')
         if successive_10 != 0:
             successive_10 -= 1
             if not b.startswith('10'):
                 return False
         elif b[0] == '1':
-                successive_10 = len(b.split('0')[0]) - 1
+            successive_10 = len(b.split('0')[0]) - 1
     return True
