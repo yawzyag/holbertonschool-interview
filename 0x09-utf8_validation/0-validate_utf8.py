778d3908b38f5b6d46e79cd5c411c220eb423b1a
yawzyag <yesid.dev93@gmail.com>
2020-09-22 02:13:52 +0000

    [Feature] another way

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,17 +4,29 @@ Validate utf8
 """
 
 
-def validUTF8(data):
-    successive_10 = 0
+def getLen(num):
+    if (num >> 7 == 0):
+        return 1
+    if (num >> 5 == 0b110):
+        return 2
+    if (num >> 4 == 0b1110):
+        return 3
+    if (num >> 3 == 0b11110):
+        return 4
+    return -1
 
-    if (len(data) == 0):
-        return False
-    for b in data:
-        b = bin(b).replace('0b', '').rjust(8, '0')
-        if successive_10 != 0:
-            successive_10 -= 1
-            if not b.startswith('10'):
+
+def validUTF8(data):
+    i = 0
+    for num in data:
+        n = getLen(num)
+        if (n == -1 or i + n > len(data)):
+            return False
+        i += 1
+        while (n > 1):
+            i += 1
+            n -= 1
+            if (data[i] >> 6 != 0b10):
                 return False
-        elif b[0] == '1':
-            successive_10 = len(b.split('0')[0]) - 1
+
     return True
