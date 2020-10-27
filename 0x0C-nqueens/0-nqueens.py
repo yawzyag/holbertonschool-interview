8575d5286ba9e05e5d62d481e66ac99072d80b24
yawzyag <yesid.dev93@gmail.com>
2020-10-27 23:01:21 +0000

    [Feature] import only sys

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,12 +3,25 @@
 [find n posible queens]
 """
 
-import sys
-import math
+from sys import argv, exit
+
+
+def ln(x):
+    n = 1000.0
+    return n * ((x ** (1/n)) - 1)
 
 
 def solveBoard(list_items, row, rowmask,
                ldmask, rdmask):
+    """[solve board]
+
+    Args:
+        list_items ([type]): [list of items in board]
+        row ([type]): [row of board]
+        rowmask ([type]): [mask]
+        ldmask ([type]): [mask]
+        rdmask ([type]): [mask]
+    """
 
     n = len(list_items)
 
@@ -31,7 +44,7 @@ def solveBoard(list_items, row, rowmask,
 
     while (safe > 0):
         p = safe & (-safe)
-        col = (int)(math.log(p)/math.log(2))
+        col = (int)(ln(p)/ln(2))
         list_items[row][col] = 1
 
         solveBoard(list_items, row+1, rowmask | p,
@@ -42,23 +55,28 @@ def solveBoard(list_items, row, rowmask,
         list_items[row][col] = 0
 
 
-if (len(sys.argv) != 2):
-    print("Usage: nqueens N")
-    sys.exit(1)
-num = 0
-try:
-    num = int(sys.argv[1])
-except:
-    print("N must be a number")
-    sys.exit(1)
+def main(argv):
+    if (len(argv) != 2):
+        print("Usage: nqueens N")
+        exit(1)
+    num = 0
+    try:
+        num = int(argv[1])
+    except:
+        print("N must be a number")
+        exit(1)
 
-if (num < 4):
-    print("N must be at least 4")
-    sys.exit(1)
-list_items = []
-i = 0
-while i < num:
-    list_items.append([0] * num)
-    i += 1
+    if (num < 4):
+        print("N must be at least 4")
+        exit(1)
+    list_items = []
+    i = 0
+    while i < num:
+        list_items.append([0] * num)
+        i += 1
 
-solveBoard(list_items, 0, 0, 0, 0)
+    solveBoard(list_items, 0, 0, 0, 0)
+
+
+if __name__ == "__main__":
+    main(argv)
