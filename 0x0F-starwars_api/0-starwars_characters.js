ba9a59559b454154ac2d27a59bd91e76a4e8906e
yawzyag <yesid.dev93@gmail.com>
2020-11-17 21:32:47 +0000

    [Feature] url

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-const textA = `http://swapi.co/api/films/${process.argv[2]}`;
+const textA = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 function doRequest (url) {
   return new Promise(function (resolve, reject) {
@@ -19,10 +19,12 @@ function doRequest (url) {
 async function main (url) {
   const res = await doRequest(url);
   const json = JSON.parse(res);
-  for (const char of json.characters) {
-    const res1 = await doRequest(char);
-    const json1 = JSON.parse(res1);
-    console.log(json1.name);
+  if (json && json.characters) {
+    for (const char of json.characters) {
+      const res1 = await doRequest(char);
+      const json1 = JSON.parse(res1);
+      console.log(json1.name);
+    }
   }
 }
 
