da5dba70957f9630ec8d3c862d8ba072b6cc8658
yawzyag <yesid.dev93@gmail.com>
2020-12-01 22:51:04 +0000

    [Feature] heap sort

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -12,43 +12,37 @@ void heap_sort(int *array, size_t size)
 	int t;
 
 	for (;;)
-	{ /* Loops until arr is sorted */
+	{
 		if (i > 0)
-		{		      /* First stage - Sorting the heap */
-			i--;	      /* Save its index to i */
-			t = array[i]; /* Save parent value to t */
+		{
+			i--;
+			t = array[i];
 		}
 		else
-		{	     /* Second stage - Extracting elements in-place */
-			n--; /* Make the new heap smaller */
+		{
+			n--;
 			if (n == 0)
-				return;	     /* When the heap is empty, we are done */
-			t = array[n];	     /* Save last value (it will be overwritten) */
-			array[n] = array[0]; /* Save largest value at the end of array */
+				return;
+			t = array[n];
+			array[n] = array[0];
 		}
-
-		parent = i;	   /* We will start pushing down t from parent */
-		child = i * 2 + 1; /* parent's left child */
-
-		/* Sift operation - pushing the value of t down the heap */
+		parent = i;
+		child = i * 2 + 1;
 		while (child < n)
 		{
 			if (child + 1 < n && array[child + 1] > array[child])
-			{
-				child++; /* Choose the largest child */
-			}
+				child++;
+
 			if (array[child] > t)
-			{				      /* If any child is bigger than the parent */
-				array[parent] = array[child]; /* Move the largest child up */
-				parent = child;		      /* Move parent pointer to this child */
-				child = parent * 2 + 1;	      /* the previous line is wrong*/
+			{
+				array[parent] = array[child];
+				parent = child;
+				child = parent * 2 + 1;
 			}
 			else
-			{
-				break; /* t's place is found */
-			}
+				break;
 		}
-		array[parent] = t; /* We save t in the heap */
+		array[parent] = t;
 		print_array(array, size);
 	}
 }
